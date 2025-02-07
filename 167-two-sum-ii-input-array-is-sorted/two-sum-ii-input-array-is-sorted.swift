3fdbaa4a353aa2313595f9f5f9d126ddb3ca237f
Sawan Kumar <sawanmind91@outlook.com>
2025-02-07 15:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (48.77%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.swift
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.swift
@@ -1,5 +1,27 @@
 class Solution {
     func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        optimised(numbers,target)
+    }
+
+    func optimised(_ numbers: [Int], _ target: Int) -> [Int] {
+        var l = 0
+        var r = numbers.count - 1
+
+        while l < r {
+            let sum = numbers[l] + numbers[r]
+
+            if sum == target {
+                return [l+1, r+1]
+            }else if sum < target {
+                l += 1
+            }else {
+                r -= 1
+            }
+        }
+        return [-1,-1]
+    }
+
+    func bruteforce(_ numbers: [Int], _ target: Int) -> [Int] {
         for i in 0..<numbers.count {
             for j in (i+1)..<numbers.count {
                 if numbers[i] + numbers[j] == target {
