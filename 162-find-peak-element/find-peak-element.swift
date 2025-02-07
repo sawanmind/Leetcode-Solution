2d5fc1d025c268b86bf9a3fd06ec1b6fc20f6f95
Sawan Kumar <sawanmind91@outlook.com>
2025-02-07 10:12:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (23.47%) - LeetSync

--- 162-find-peak-element/find-peak-element.swift
+++ 162-find-peak-element/find-peak-element.swift
@@ -1,17 +1,17 @@
 class Solution {
     func findPeakElement(_ nums: [Int]) -> Int {
-        optimised(nums)
+        bruteforce(nums)
     }
 
     func bruteforce(_ nums: [Int]) -> Int {
-        let n = nums.count
-
-        if n == 1 {return 0}
+        if nums.count == 1 {
+            return 0
+        }
 
         for i in 0..<nums.count {
             if i == 0 {
                 if nums[i] > nums[i+1] {return i}
-            }else if i == n - 1 {
+            }else if i == nums.count - 1 {
                 if nums[i] > nums[i-1] {return i}
             }else {
                 if nums[i] > nums[i-1] && nums[i] > nums[i+1] {return i}
@@ -19,23 +19,5 @@ class Solution {
         }
 
         return -1
-    }
-
-    func optimised(_ nums: [Int]) -> Int {
-        let n = nums.count
-        var left = 0
-        var right = n - 1
-
-        while left < right {
-            let mid = left + (right - left)/2
-
-            if nums[mid] < nums[mid+1] {
-                left = mid + 1
-            }else {
-                right = mid
-            }
-        }
-        
-        return left
     }
 }
