2a1dede1651aa1129086390eada280a5fd23c233
Sawan Kumar <sawanmind91@outlook.com>
2025-02-07 10:04:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (33.41%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.swift
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.swift
@@ -1,29 +1,31 @@
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        var left = 0
-        var right = nums.count - 1
-        var result = -1
+        binarysearch(nums, target)
+    }
 
-        while left <= right {
-            let mid = left + (right - left)/2
-            
-            if nums[mid] == target {
-                return mid
-            }else if nums[right] >= nums[mid] {
-                if nums[right] >= target && nums[mid] < target {
-                    left = mid + 1
+    func binarysearch(_ nums: [Int], _ target: Int) -> Int {
+        var l = 0 // left
+        var r = nums.count - 1 // right
+
+        while l <= r {
+            let m = l + (r - l)/2 // mid
+
+            if nums[m] == target {
+                return m
+            }else if nums[r] >= nums[m] {
+                if nums[r] >= target && nums[m] < target {
+                    l = m + 1
                 }else {
-                    right = mid - 1
+                    r = m - 1
                 }
             }else {
-                if nums[left] <= target && nums[mid] > target {
-                    right = mid - 1
+                if nums[l] <= target && nums[m] > target {
+                    r = m - 1
                 }else {
-                    left = mid + 1
+                    l = m + 1
                 }
             }
         }
-
-        return result
+        return -1
     }
 }
