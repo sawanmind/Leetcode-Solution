fe3de4b9a73299284a088838f9c54ab437440a79
Sawan Kumar <sawanmind91@outlook.com>
2025-02-13 10:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (14.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.swift
+++ 70-climbing-stairs/climbing-stairs.swift
@@ -1,30 +1,16 @@
 class Solution {
-
     func climbStairs(_ n: Int) -> Int {
-        optimised(n)
-    } 
-
-    func bruteforce(_ n: Int) -> Int { // TLE
         if n == 1 {return 1}
-        if n == 2 {return 2}
-
-        return bruteforce(n - 1) + bruteforce(n - 2)
-    }
-
-    func optimised(_ n: Int) -> Int {
-        if n == 1 {return 1}
-        if n == 2 {return 2}
+        if n == 2 { return 2}
 
         var dp = Array(repeating: 0, count: n + 1)
         dp[1] = 1
         dp[2] = 2
 
         for i in 3...n {
-            dp[i] = dp[i-1] + dp[i-2]
+            dp[i] = dp[i - 1] + dp[i - 2]
         }
-        
+
         return dp[n]
     }
-
-
 }
