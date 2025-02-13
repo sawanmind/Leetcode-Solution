90a8b0d00dba500cfe455f2233c76981422df7d9
Sawan Kumar <sawanmind91@outlook.com>
2025-02-13 11:06:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (34.91%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func rob(_ nums: [Int]) -> Int {
+        guard !nums.isEmpty else {return 0}
+        if nums.count == 1 {return nums[0]}
+        if nums.count == 2 {return max(nums[0], nums[1])}
+
+        let rob1 = robber(Array(nums[0..<nums.count - 1])) 
+        let rob2 = robber(Array(nums[1..<nums.count])) 
+        return max(rob1, rob2)
+    }
+
+    func robber(_ nums: [Int]) -> Int {
+        guard !nums.isEmpty else {return 0}
+        if nums.count == 1 {return nums[0]}
+        if nums.count == 2 {return max(nums[0], nums[1])}
+
+        var dp = Array(repeating: 0, count: nums.count)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in 2..<nums.count {
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        }
+
+        return dp[nums.count - 1]
+    }
+}
