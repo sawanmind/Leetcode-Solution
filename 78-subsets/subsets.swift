735b3ee29f48e760d8a5e27f6659105d82aad6f8
Sawan Kumar <sawanmind91@outlook.com>
2025-02-13 10:33:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (8.42%) - LeetSync

---
+++ 78-subsets/subsets.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        var output = [Int]()
+
+        func solve(_ start: Int, _ output: inout [Int]) {
+            result.append(output)
+
+
+            for i in start..<nums.count {
+                output.append(nums[i])
+                solve(i + 1, &output)
+                output.removeLast()
+            }
+
+            
+        }
+
+        solve(0, &output)
+
+        return result
+    }
+}
