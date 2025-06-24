1cef2dcf839dcc2f8124d45e1a63901c832ac9c8
Sawan Kumar <sawanmind91@outlook.com>
2025-06-24 10:10:55 +0530

    Time: 20 ms (42.45%) | Memory: 22.4 MB (62.72%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.swift
+++ 217-contains-duplicate/contains-duplicate.swift
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     func containsDuplicate(_ nums: [Int]) -> Bool {
         var dic = [Int: Int]()
 
@@ -7,5 +8,17 @@ class Solution {
         }
 
         return !dic.filter {$0.value >= 2}.isEmpty
+    }
+    */
+
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var set = Set<Int>()
+
+        for num in nums {
+            if set.contains(num) {return true}
+            else { set.insert(num) }
+        }
+
+        return false
     }
 }
